d91393c244d567394109bdab114740179540cff0
Pratik110320 <praut1103@gmail.com>
2025-07-16 19:09:29 +0530

    Initial commit with .gitignore

---
+++ src/main/java/com/pratik/IotAnalyser/IotAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.pratik.IotAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotAnalyserApplication.class, args);
+	}
+
+}
